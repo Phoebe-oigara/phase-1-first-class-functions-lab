b55475485e9c357db12c381a8bd0770fd5c1428f
Phoebe Oigara <phoebeoigara@gmail.com>
2023-03-16 20:16:49 +0300

    Exercised with various implementaions of first class functions.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers,
+];
+
+function createFareMultiplier(multiply) {
+    return fare=> {
+        return multiply*fare;}
+}
+
+const fareDoubler=createFareMultiplier(2);
+
+const fareTripler=createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, driversFunc) {
+   if (driversFunc=== returnFirstTwoDrivers) {
+    return drivers.slice(0,2); 
+ }
+    else if (driversFunc === returnLastTwoDrivers){
+        return drivers.slice(-2);
+    }
+}
+
